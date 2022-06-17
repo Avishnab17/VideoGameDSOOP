f26f5435d5e511e3010f30af3f4bde0f5b172944
Vaishnavi <Vaishnavi1009>
2022-06-17 23:36:01 +0400

    Write printMenu

--- src/videogame/main/GameLogic.java
+++ src/videogame/main/GameLogic.java
@@ -59,42 +59,6 @@ import java.util.Scanner;
         }
         //method gameLoop
 
-        public static void startGame(){
-            boolean nameSet=false;
-            String name;
-            //print title screen
-            clearConsole();
-            printSeperator(40);
-            printSeperator(30;
-            System.out.println(" AGE OF EVIL EMPEROR ");
-            printSeperator(30);
-            printSeperator(40);
-            anythingToContinue();
-
-            do{
-                clearConsole();
-                printHeading("Whats's your name ?");
-                name=scanner.next();
-                clearConsole();
-                printHeading("Your name is "+name"./Is that correct?");
-                System.out.println("(1) Yes!");
-                System.out.println("(2) No,I want to change my name.");
-                int input=readInt("->",2);
-                if(input==1)
-                    nameSet=true;
-            }while(!nameSet);
-            //creating player object with name
-            player=new Player(name);
-
-            //print first story intro
-            Story.printFirstActIntro();
-
-            //set Running to true for game loop to continue
-            isRunnining = true;
-
-            //start main game loop
-            gameLoop();
-        }
 
         //method checkAct
 //method continueJourney
