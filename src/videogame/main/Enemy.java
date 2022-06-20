782fcece3e535775abe851ad6cf389b5a7702feb
Vrisht-Raaj <vrisht-raaj.joyseeree@umail.uom.ac.mu>
2022-06-20 20:44:41 +0400

    Enemy Class beta build

--- src/videogame/main/Enemy.java
+++ src/videogame/main/Enemy.java
@@ -1,4 +1,26 @@
 package videogame.main;
-//
-public class Enemy {
+
+public class Enemy extends Character{
+
+  //variable to store player's current XP
+  int playerXp;
+
+  //Enemy-specific constructor
+  public Enemy(String Name, int playerXp){
+
+      super(name, (int)(Math.random() * playerXp + playerXp/3 + 5), (int)(Math.random()*(playerXp/4 + 2)+1));
+        //assigning variable
+       this.playerXp = playerXp;
+
+  }
+  //Enemy specific attack and defense calculations
+  @Override
+  public int attack(){
+      return 0;
+  }
+  @Override
+    public int defend(){
+        return 0;
+    }
+
 }
