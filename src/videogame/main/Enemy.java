ea92419f91ffbbd1ae7fe391d4c6a7376f2a92b1
Avishna <bootunavishna5@gmail.com>
2022-06-21 18:01:06 +0400

    changed formula

--- src/videogame/main/Enemy.java
+++ src/videogame/main/Enemy.java
@@ -8,7 +8,7 @@ public class Enemy extends Character{
   //Enemy-specific constructor
   public Enemy(String name, int playerXp){
 
-      super(name, (int)(Math.random() *playerXp + playerXp/3 + 5), (int)(Math.random()*(playerXp/4 + 2)+1));
+      super(name, (int)(Math.random()*playerXp + playerXp/3 + 5), (int) (Math.random()*(playerXp/4 + 2)+1));
         //assigning variable
        this.playerXp = playerXp;
 
@@ -16,11 +16,11 @@ public class Enemy extends Character{
   //Enemy specific attack and defense calculations
   @Override
   public int attack(){
-      return (int)(Math.random()*(playerXp/4 + 1) + xp/4 + 3);
+      return (int) (Math.random()*(playerXp/4 + 1) + xp/4 + 3);
   }
   @Override
     public int defend(){
-        return (int)(Math.random()*(playerXp/4 + 1) + xp/4 + 3);
+        return (int) (Math.random()*(playerXp/4 + 1) + xp/4 + 3);
     }
 
 }
