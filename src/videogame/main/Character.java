b1b7873a5e9b1e431e20a9527eb7b1a947deeeb1
Avishna <bootunavishna5@gmail.com>
2022-06-23 19:43:20 +0400

    added comments in player

--- src/videogame/main/Character.java
+++ src/videogame/main/Character.java
@@ -1,13 +1,13 @@
 package videogame.main;
 
-public abstract class Character
+public abstract class Character  //both player and enemy will inherit from
 {
     //Variables / Attributes all characters have
-    public String name;
-    public int maxHp,hp,xp;
+    public String name; //stores character's name
+    public int maxHp, hp, xp;
 
     //Constructor for character
-    public Character(String name,int maxHp,int xp)
+    public Character(String name, int maxHp, int xp) //constructor
     {
         this.name=name;
         this.maxHp=maxHp;
@@ -15,7 +15,7 @@ public abstract class Character
         this.hp=maxHp;
 
     }
-    public abstract int attack();
+    public abstract int attack();  //abstract and will be overwritten by the subclasses
     public abstract int defend();
 }
 
