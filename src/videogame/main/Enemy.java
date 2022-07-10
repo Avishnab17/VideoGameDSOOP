d11299834d436ed08e2bb7c6c46d82820b009ef8
Vaishnavi <Vaishnavi1009>
2022-07-10 15:42:24 +0400

    change random for player and enemy

--- src/videogame/main/Enemy.java
+++ src/videogame/main/Enemy.java
@@ -16,7 +16,7 @@ public class Enemy extends Character{
   //Enemy specific attack and defense calculations
   @Override
   public int attack(){
-      return (int) (Math.random()*(playerXp/2 + 1) + xp/2 + 3);
+      return (int) (Math.random()*(playerXp/2 + +5) + xp/2 + 5);
   }
   @Override
     public int defend(){
