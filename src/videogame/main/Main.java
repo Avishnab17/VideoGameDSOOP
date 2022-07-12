69529065bede396fc6dd1ec71c33c1c300b88ebb
Avishna <bootunavishna5@gmail.com>
2022-07-12 08:29:37 +0400

    javadoc comments in main

--- src/videogame/main/Main.java
+++ src/videogame/main/Main.java
@@ -1,4 +1,8 @@
 package videogame.main;
+        /**
+        * This class calls the GameLogic class
+        * @author Avishna
+        */
 public class Main{
     public static void main(String[] args){
         //start game
