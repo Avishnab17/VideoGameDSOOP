531525fff8e54f6b765c75eeaa041231a42eba49
yashi <yashinayurvi@gmail.com>
2022-07-14 13:35:03 +0400

    generated new javadoc

--- JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"videogame.main","l":"Character"},{"p":"videogame.main","l":"Enemy"},{"p":"videogame.main","l":"GameLogic"},{"p":"videogame.main","l":"Main"},{"p":"videogame.main","l":"Player"},{"p":"videogame.main","l":"Story"}];updateSearchResults();
